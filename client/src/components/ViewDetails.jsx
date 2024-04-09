70418e8432ea5f5c561f59c27d4f71ebf11828ab
arandel1 <allison.randel@gmail.com>
2024-04-08 22:02:48 -0500

    added price to view details

--- client/src/components/ViewDetails.jsx
+++ client/src/components/ViewDetails.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 const ViewDetails = ({ products }) => {
+
   const navigate = useNavigate();
   const params = useParams();
   const id = +params.productId;
@@ -10,12 +11,13 @@ const ViewDetails = ({ products }) => {
     return <div>Product not found</div>;
   }
 
-
   return(
     <>
       <div className='single-product-container'>
         <h2>{product.name}</h2>
+        <h2>${product.price}</h2>
         <p>{product.desc}</p>
+
         <img className = 'product-image' src={product.imgURL} alt = 'product image'/>
         <button onClick = {() => navigate(`/products`)}>Back to All Products</button>
         <button onClick={()=> navigate(`/order`)}>Order</button>
