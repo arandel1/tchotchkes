0b382c148b80147b1e724daa53ced727efc88075
Ali Hager <ahager2013@gmail.com>
2024-04-16 12:35:02 -0400

    logout button working in login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Logout } from "./Logout";
 
-const baseUrl = 'http://localhost:8080/tchotchke/users'
+const baseUrl = "http://localhost:8080/tchotchke/users";
 
-function Login( {auth} ) {
+function Login({ auth }) {
   const [formData, setFormData] = useState({
-    email:'', 
-    password:''
+    email: "",
+    password: "",
   });
 
   // const navigate = useNavigate();
@@ -16,75 +17,77 @@ function Login( {auth} ) {
   const [successMessage, setSuccessMessage] = useState(null);
 
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.name]: e.target.value});
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async(e)=> {
+  const handleLogout = () => {
+    Logout();
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // await login(formData)
-     try {
+    try {
       const response = await fetch(`${baseUrl}/login`, {
-        method: 'POST', 
-        headers: { 'Content-Type': 'application/json' },    
-        body: JSON.stringify(formData)
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
       });
 
-      if(response.ok){
+      if (response.ok) {
         const user = await response.json();
         setSuccessMessage("You're logged in!");
-        setFormData({ email: formData.email, password:'' })
+        setFormData({ email: formData.email, password: "" });
         // navigate('/products');
         // console.log(formData)
         // console.log(response)
         // console.log(user)
         const token = user.token;
         // console.log(token)
-        auth(user)
-        localStorage.setItem("auth", JSON.stringify(user))
-
+        auth(user);
+        localStorage.setItem("auth", JSON.stringify(user));
       } else {
         const errorData = await response.json();
-        setErrorMessage(errorData.message || 'Login failed. Please try again.')
+        setErrorMessage(errorData.message || "Login failed. Please try again.");
       }
-    }
-    catch (error){
-      console.error('Error:', error); 
-      setErrorMessage('Login failed. Check your username or password.');
+    } catch (error) {
+      console.error("Error:", error);
+      setErrorMessage("Login failed. Check your username or password.");
     }
   };
 
   return (
-    <div className = 'container'>
+    <div className="container">
       <h3>Login</h3>
-      <form onSubmit = { handleSubmit }>
-        <label
-        className='email'>Email:</label>
-          <input 
-            type='email'
-            name='email'
-            placeholder = 'Email'
-            value = { formData.email }
-            onChange = { handleChange }
-          />
+      <form onSubmit={handleSubmit}>
+        <label className="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
         <br />
 
         <label className="password">Password:</label>
-            <input
-              type='password'
-              name='password'
-              placeholder='Password'
-              value={formData.password}
-              onChange={ handleChange }  
-            />
-          <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <br />
 
-        <button type='submit'>Login</button>
+        <button type="submit">Login</button>
+        <button onClick={handleLogout}>Logout</button>
 
-        {errorMessage && <p className='error-message'>{errorMessage}</p>}
-        {successMessage && <p className='success-message'>{successMessage}</p>}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        {successMessage && <p className="success-message">{successMessage}</p>}
       </form>
     </div>
   );
-};
+}
 
 export default Login;
