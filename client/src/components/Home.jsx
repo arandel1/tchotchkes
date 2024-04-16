d09a61dd4069f996b94038781c67575019f9bd6a
katyjayne <katyjayneblank@gmail.com>
2024-04-16 15:10:03 -0400

    formatted it to the best of my ability for now

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,5 @@
 ﻿import React from "react";
-import { Carousel } from "react-bootstrap";
+import { Carousel, CarouselItem, CarouselCaption } from "react-bootstrap";
 import AeroTransistorRadio from '../assets/Carousel-Images/AeroTransistorRadio.png';
 import AlladinTableLamp from '../assets/Carousel-Images/AlladinTableLamp.png';
 import BeatrixPotterBook from '../assets/Carousel-Images/BeatrixPotterBook.png';
@@ -11,45 +11,46 @@ import PollyGasNeon from '../assets/Carousel-Images/PollyGasNeon.png';
 
 export default function Home () {
   return (
+    
     <div>
-      <h2>Carousel of Featured Products</h2>
-      <Carousel data-bs-theme="dark">
-        <Carousel.Item>
-          <img className="d-block w-100" src={AeroTransistorRadio} alt="Transistor Radio" />
-          <Carousel.Caption>
-            <h3>Aero Transistor Radio</h3>
-            <p>Vintage Aero Transistor Radio, circa 1960s</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={AlladinTableLamp} alt="Lamp Shaped Table Lighter" />
-          <Carousel.Caption>
-            <h3>Alladin Lamp Table Lighter</h3>
-            <p>Vintage Aladdin Table Lamp-Style Cigarette Lighter, Made In Japan</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={BeatrixPotterBook} alt="Vintage Book of Nursery Rhymes" />
-          <Carousel.Caption>
-            <h3>Cecily Parsley's Nursery Rhymes Book</h3>
-            <p>By Beatrix Potter, First Edition, circa 1922</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Cameo} alt="Cameo Brooch" />
-          <Carousel.Caption>
-            <h3>Perfume Brooch</h3>
-            <p>Vintage "Perfume" Cameo in Original Box</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={PollyGasNeon} alt="Neon Parrot Sign" />
-          <Carousel.Caption>
-            <h3>Neon Polly Gas Sign</h3>
-            <p>Vintage Polly Gas Parrot Sign, circa 1950s</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-    </Carousel>
+       <h2 id="carouselHeading" className="carousel-heading d-block w-100 bg-dark text-light font-size-xl text-uppercase rounded-pill py-15 px-15 mt-5 mb-5">Featured Items</h2>
+        <Carousel className="custom-carousel" data-bs-theme="dark" style={{ display: 'flex', justifyContent: 'center' }}>
+          <CarouselItem>
+            <img className="d-block w-100 py-5 px-5 mb-5 mt-0" src={AeroTransistorRadio} alt="Transistor Radio" />
+            <CarouselCaption className="carousel-caption d-block bg-dark text-light rounded-pill w-75 py-2 mt-3 mb-2 px-3">
+              <h3 className="font-weight-bold text-uppercase">Aero Transistor Radio</h3>
+              <p>Vintage Aero Transistor Radio, circa 1960s</p>
+            </CarouselCaption>
+          </CarouselItem>
+          <CarouselItem>
+            <img className="d-block w-100 py-5 px-5 mb-5 mt-0" src={AlladinTableLamp} alt="Lamp Shaped Table Lighter" />
+            <CarouselCaption className="carousel-caption d-block bg-dark text-light rounded-pill w-75 py-2 mt-3 mb-2 px-3">
+              <h3 className="font-weight-bold text-uppercase">Alladin Lamp Table Lighter</h3>
+              <p>Vintage Aladdin Table Lamp-Style Cigarette Lighter, Made In Japan</p>
+            </CarouselCaption>
+          </CarouselItem>
+          <CarouselItem>
+            <img className="d-block w-100 py-5 px-5 mb-5 mt-0" src={BeatrixPotterBook} alt="Vintage Book of Nursery Rhymes" />
+            <CarouselCaption className="ccarousel-caption d-block bg-dark text-light rounded-pill py-2 mt-3 mb-2 px-3">
+              <h3 className="font-weight-bold text-uppercase">Cecily Parsley's Nursery Rhymes Book</h3>
+              <p>By Beatrix Potter, First Edition, circa 1922</p>
+            </CarouselCaption>
+          </CarouselItem>
+          <CarouselItem>
+            <img className="d-block w-100 py-5 px-5 mb-5 mt-0" src={Cameo} alt="Cameo Brooch" />
+            <CarouselCaption className="carousel-caption d-block bg-dark text-light rounded-pill py-2 mt-3 mb-2 px-3">
+              <h3 className="font-weight-bold text-uppercase">Perfume Brooch</h3>
+              <p>Vintage "Perfume" Cameo in Original Box</p>
+            </CarouselCaption>
+          </CarouselItem>
+          <CarouselItem>
+            <img className="d-block w-100 py-5 px-5 mb-5 mt-0" src={PollyGasNeon} alt="Neon Parrot Sign" />
+            <CarouselCaption className="carousel-caption d-block bg-dark text-light rounded-pill py-2 mt-3 mb-2 px-3">
+              <h3 className="font-weight-bold text-uppercase">Neon Polly Gas Sign</h3>
+              <p>Vintage Polly Gas Parrot Sign, circa 1950s</p>
+            </CarouselCaption>
+          </CarouselItem>
+        </Carousel>
     </div>
   );
 }
