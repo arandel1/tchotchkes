f05f287ca9bf6212ff6ae821188d8f958afb8c57
kiralom <kira.lom@gmail.com>
2024-04-18 17:33:43 -0400

    fixed code for cart item rendering and order total

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -3,7 +3,10 @@ import { useEffect } from "react";
 import RemoveItemFromCart from "./RemoveItemFromCart";
 
 export default function Cart({ auth, products }) {
+  //console.log(auth);
   const [cartItems, setCartItems] = useState([]);
+  const [orders, setOrders] = useState([]);
+  // const [cartAuth, setCartAuth] = useState();
 
   useEffect(() => {
     async function getUserProducts() {
@@ -30,7 +33,11 @@ export default function Cart({ auth, products }) {
     }
   }, [cartItems]);
 
-  // const cartTotal = (cartItems.map(item) => item.price)
+  const priceArray = cartItems.map((order) => order.price);
+  if (cartItems.length === 0) {
+    <h4>Cart is empty.</h4>;
+  }
+  const cartTotal = priceArray.reduce((total, price) => total + price, 0);
 
   return (
     <div className="container">
@@ -40,47 +47,30 @@ export default function Cart({ auth, products }) {
         <p> Your cart is empty.</p>
       ) : (
         cartItems.map((product) => {
-          const orderId =
-            cartItems.orders &&
-            cartItems.orders
-              .filter((order) => order.productsId === product.id)
-              .map((order) => order.id);
-          console.log(orderId);
+          const orderFilter = cartItems.filter(
+            (order) => order.productsId === product.id
+          );
+          const orderId = orderFilter.map((order) => order.id);
+          // const orderId =
+          //   cartItems.orders &&
+          //   cartItems.orders
+          //     .filter((order) => order.productsId === product.id)
+          //     .map((order) => order.id);
           return (
             <div key={product.orderId} className="product">
               <img height="100px" src={product.imgURL} alt={product.name}></img>
               <h5>{product.name}</h5>
               <h5>${product.price}</h5>
-              {/* <RemoveItemFromCart
+              <RemoveItemFromCart
                 orderId={orderId[0]}
                 setCartItem={setCartItems}
                 cartItems={cartItems}
-              /> */}
+              />
             </div>
           );
         })
       )}
-      {/* <h4>Cart Total: ${cartTotal}</h4> */}
-
-      {/* ({cartItems.map((product) => (
-        
-        <div key={product.id} className="product">
-          <img height="100px" src={product.imgURL} alt="thumbnail"></img>
-          <h5>{product.name}</h5>
-          <h5>${product.price}</h5>
-          <button onClick={() => navigate(`/products/${product.id}`)}>
-            View Details
-          </button>
-          <button onClick={() => removeProduct(id)}>Remove</button>
-        </div>
-      ))}
-      
       <h4>Cart Total: ${cartTotal}</h4>
-      
-      ) :  (
-      
-        <p>Loading cart ...</p>
-      ) */}
     </div>
   );
 }
