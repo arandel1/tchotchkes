02766d5635fd8e5a206d6ea051f1f87eb559155b
arandel1 <allison.randel@gmail.com>
2024-04-19 11:01:58 -0500

    cart price total is appearing and accurate

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -13,7 +13,7 @@ export default function Cart({ auth, products }) {
       const baseUrl = "http://localhost:8080/tchotchke";
       try {
         const cartAuth = !auth.id ? JSON.parse(auth) : auth;
-        console.log(cartAuth.id);
+        // console.log(cartAuth.id);
 
         // console.log("Fetching products...");
 
@@ -33,43 +33,56 @@ export default function Cart({ auth, products }) {
     }
   }, [cartItems]);
 
-  // const priceArray = cartItems.map((order) => order.price);
-  // if (cartItems.length === 0) {
-  //   <h4>Cart is empty.</h4>;
-  // }
-  // const cartTotal = priceArray.reduce((total, price) => total + price, 0);
-
-  console.log("cartItems.products", cartItems.products);
-  console.log("cartItems.orders", cartItems.orders);
   
+
+  // console.log("cartItems.products", cartItems.products);
+  // console.log("cartItems.orders", cartItems.orders);
+  
+  const productArray = cartItems.products || []
+  const orderArray = cartItems.orders || []
+
+  console.log(productArray)
+
+  const priceArray = productArray.map((products) => products.price);
+  console.log("price Array:", priceArray)
+  const cartTotal = priceArray.reduce((total, price) => total + price, 0);
+
+ 
+
   return (
-    <div className="container">
-      <h2>Your Cart</h2>
-      {/*{console.log("Cart items length:", cartItems.products.length)}*/}
-      {cartItems.length === 0 ? (
-        <p> Your cart is empty.</p>
-      ) : (
-        cartItems.products.map((product) => {
-          const orderId =
-            cartItems.orders &&
-            cartItems.orders
-              .filter((order) => order.productsId === product.id)
-              .map((order) => order.id);
-          console.log(orderId);
-          return (
-            <div key={product.orderId} className="product">
-              <img height="100px" src={product.imgURL} alt={product.name}></img>
-              <h5>{product.name}</h5>
-              <h5>${product.price}</h5>
-              <RemoveItemFromCart
-                orderId={orderId[0]}
-                setCartItem={setCartItems}
-                cartItems={cartItems}
-              />
-            </div>
-          );
-        })
-      )}
-      </div>
+    <>
+      <div className="container">
+        <h2>Your Cart</h2>
+        {/*{console.log("Cart items length:", cartItems.products.length)}*/}
+        {cartItems.length === 0 ? (
+          <p> Your cart is empty.</p>
+        ) : (
+          productArray.map((product) => {
+            const orderId =
+              orderArray &&
+              orderArray
+                .filter((order) => order.productsId === product.id)
+                .map((order) => order.id);
+            console.log(orderId);
+            return (
+              <div key={product.orderId} className="product">
+                <img height="100px" src={product.imgURL} alt={product.name}></img>
+                <h5>{product.name}</h5>
+                <h5>${product.price}</h5>
+                <RemoveItemFromCart
+                  orderId={orderId[0]}
+                  setCartItem={setCartItems}
+                  cartItems={cartItems}
+                />
+              </div>
+            );
+          })
+        )}
+        </div>
+        <br/>
+        <div className="price-total">
+          <h4>Cart Total: ${cartTotal}</h4>
+        </div>
+    </>
   );
 }
