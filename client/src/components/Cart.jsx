d16b97851ccdb199ae36c936704615e92ae4d156
arandel1 <allison.randel@gmail.com>
2024-04-19 10:35:17 -0500

    bugs fixed, cart is rendering, working on getting priceArray back up and running

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -33,16 +33,15 @@ export default function Cart({ auth, products }) {
     }
   }, [cartItems]);
 
-  console.log(cartItems)
-
-  const priceArray = cartItems.map((order) => order.price);
-  if (cartItems.length === 0) {
-    <h4>Cart is empty.</h4>;
-  }
-  const cartTotal = priceArray.reduce((total, price) => total + price, 0);
+  // const priceArray = cartItems.map((order) => order.price);
+  // if (cartItems.length === 0) {
+  //   <h4>Cart is empty.</h4>;
+  // }
+  // const cartTotal = priceArray.reduce((total, price) => total + price, 0);
 
   console.log("cartItems.products", cartItems.products);
   console.log("cartItems.orders", cartItems.orders);
+  
   return (
     <div className="container">
       <h2>Your Cart</h2>
@@ -50,17 +49,13 @@ export default function Cart({ auth, products }) {
       {cartItems.length === 0 ? (
         <p> Your cart is empty.</p>
       ) : (
-        cartItems.map((product) => {
-          const orderFilter = cartItems.filter(
-            (order) => order.productsId === product.id
-          );
-          const orderId = orderFilter.map((order) => order.id);
-          // const orderId =
-          //   cartItems.orders &&
-          //   cartItems.orders
-          //     .filter((order) => order.productsId === product.id)
-          //     .map((order) => order.id);
-
+        cartItems.products.map((product) => {
+          const orderId =
+            cartItems.orders &&
+            cartItems.orders
+              .filter((order) => order.productsId === product.id)
+              .map((order) => order.id);
+          console.log(orderId);
           return (
             <div key={product.orderId} className="product">
               <img height="100px" src={product.imgURL} alt={product.name}></img>
@@ -75,23 +70,6 @@ export default function Cart({ auth, products }) {
           );
         })
       )}
-
-      {/* <h4>Cart Total: ${cartTotal}</h4> */}
-
-      {/* ({cartItems.map((product) => (
-        
-        <div key={product.id} className="product">
-          <img height="100px" src={product.imgURL} alt="thumbnail"></img>
-          <h5>{product.name}</h5>
-          <h5>${product.price}</h5>
-          <button onClick={() => navigate(`/products/${product.id}`)}>
-            View Details
-          </button>
-          <button onClick={() => removeProduct(id)}>Remove</button>
-        </div>
-      ))}
-      
-    <h4>Cart Total: ${cartTotal}</h4>*/}
-    </div>
+      </div>
   );
 }
