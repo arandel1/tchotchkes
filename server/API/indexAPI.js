b53372ee38656f77b6acff8d39b5a592a75802cb
kiralom <kira.lom@gmail.com>
2024-04-19 21:05:23 -0400

    something dumb

--- server/API/indexAPI.js
+++ server/API/indexAPI.js
@@ -1,11 +1,12 @@
-const express = require("express")
-const apiRouter = express.Router()
+const express = require("express");
+const apiRouter = express.Router();
 
-apiRouter.use("/products", require("./products"))
-module.exports = apiRouter
+apiRouter.use("/products", require("./products"));
+module.exports = apiRouter;
 
-apiRouter.use("/users", require("./users"))
-module.exports = apiRouter
+apiRouter.use("/users", require("./users"));
+module.exports = apiRouter;
 
-apiRouter.use("/orders", require("./orders"))
-module.exports = apiRouter
+apiRouter.use("/orders", require("./orders"));
+module.exports = apiRouter;
+console.log("something dumb");
